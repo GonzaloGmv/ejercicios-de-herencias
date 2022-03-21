167b3e3aafddc9c96ff7edd058b1182b8ec6bdbf
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-03-21 15:53:08 +0100

    Dos clases derivadas metidas en el programa

--- clases/ejr_c.py
+++ clases/ejr_c.py
@@ -1,3 +1,14 @@
+#CLASE BASE
 class A:
     def __init__(self,a):
         self.a=a
+
+#CLASE DERIVADA DE LA CLASE A
+class B(A):
+    def __init__(self,b):
+        self.b=b
+
+#CLASE DERIVADA DE LA CLASE A
+class C(A):
+    def __init__(self,c):
+        self.c=c
