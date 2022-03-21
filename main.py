30164709f4d90f57cfd92294282ec350a921113e
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-03-21 16:31:30 +0100

    main

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from clases.ejr_a import *
+from clases.ejr_c import ejercicio_c
 from clases.herenciamultiplecasoreal import *
+
 if __name__ == "__main__":
     while True:
         ejr = input("Escriba el numero del ejercicio que desea iniciar, a, c o d: ")
@@ -20,5 +22,7 @@ if __name__ == "__main__":
         c = Punto3D(1,5,-3)
         c.traslacion(0, -2, 1)
         print("C = {}".format(c)) 
+    if (ejr =='c' or ejr =='C') :
+        ejercicio_c
     if ejr =='d':
         ejercicio = herenciamultiplecasoreal()
