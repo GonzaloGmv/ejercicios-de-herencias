f21f74d4a4e07d42c57f8d1b82a6357da50c002c
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-03-21 16:24:03 +0100

    par de cosas

--- clases/ejr_c.py
+++ clases/ejr_c.py
@@ -12,3 +12,11 @@ class B(A):
 class C(A):
     def __init__(self,c):
         self.c=c
+
+class D(B,C):
+    def __init__(self,d):
+        self.d=d
+
+d = D(1,2,3)
+
+print("Pertenece el elemento d a la clase A?: {}\nPertenece el elemento d a la clase B?: {}\nPertenece el elemento d a la clase C?: {}".format(isinstance(d,A),isinstance(d,B),isinstance(d,C)))
