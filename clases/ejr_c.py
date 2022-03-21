73a6d1d570c94684873df302fe89f242e1df3b5d
migueliiin <91721552+migueliiin@users.noreply.github.com>
2022-03-21 16:28:00 +0100

    ejercicio hecho

--- clases/ejr_c.py
+++ clases/ejr_c.py
@@ -2,21 +2,29 @@
 class A:
     def __init__(self,a):
         self.a=a
+        a=A
 
 #CLASE DERIVADA DE LA CLASE A
 class B(A):
-    def __init__(self,b):
+    def __init__(self,a,b):
         self.b=b
+        self.a=a
 
 #CLASE DERIVADA DE LA CLASE A
 class C(A):
-    def __init__(self,c):
+    def __init__(self,a,c):
         self.c=c
+        self.a=a
 
+#CLASE DERIVADA DE LA CLASE A
 class D(B,C):
-    def __init__(self,d):
-        self.d=d
+    def __init__(self,a,b,c):
+        self.a=a
+        self.b=b
+        self.c=c
 
 d = D(1,2,3)
 
 print("Pertenece el elemento d a la clase A?: {}\nPertenece el elemento d a la clase B?: {}\nPertenece el elemento d a la clase C?: {}".format(isinstance(d,A),isinstance(d,B),isinstance(d,C)))
+
+print("a(A)={} \nb(B)={} \nc(C)={} \n".format(str(d.a),str(d.b),str(d.c)))
